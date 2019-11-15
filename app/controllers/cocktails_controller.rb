d72ca37a1238b94462b05e19b0a1318ebcebd984
Remy Phan <phan.remy@gmail.com>
2019-11-15 17:02:13 +0100

    12

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,6 +5,11 @@ class CocktailsController < ApplicationController
 
   def index
     @cocktails = Cocktail.all
+    @search = params["search"]
+    if @search.present?
+      @name = @search["name"]
+      @cocktails = Cocktail.where(name: @name)
+    end
   end
 
   def show
--- app/javascript/components/select.js
+++ app/javascript/components/select.js
@@ -4,6 +4,5 @@ import 'select2/dist/css/select2.min.css';
 
 const initSelect2 = () => {
   $('.ingredient-select').select2();
+  $('.cocktail-select').select2();
 }
-
-export { initSelect2 };
