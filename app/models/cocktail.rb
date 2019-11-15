22a5f2ace10a21ebea1e86d26ce35386a7e60b5d
Remy Phan <phan.remy@gmail.com>
2019-11-15 11:42:31 +0100

    4nd push

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,5 @@ class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  has_one :photo
+  has_one_attached :photo
 end
